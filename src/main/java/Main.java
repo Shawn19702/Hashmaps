18d5485e6ed71245c6e0fc8820df173217747b88
Shawn19702 <112501775+Shawn19702@users.noreply.github.com>
2022-10-10 15:35:42 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,44 +4,53 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("Bob");
 
-        PhoneNumber phoneNumber = new PhoneNumber(80887);
+        PhoneNumber phoneNumber = new PhoneNumber(986);
+
+        Person person1 = new Person("Johnny");
+
+        PhoneNumber phoneNumbere = new PhoneNumber(80696887);
 
 
         HashMap<Person, PhoneNumber> personPhoneNumberHashMap = new HashMap<>();
         personPhoneNumberHashMap.put(person, phoneNumber);
 
+        personPhoneNumberHashMap.put(person1,phoneNumbere);
 
-        for (var entry : personPhoneNumberHashMap.entrySet()) {
-            System.out.println(entry.getValue());
 
+        for (PhoneNumber i : personPhoneNumberHashMap.values()) {
+            System.out.println(i);
+        }
+
+            System.out.println("=====================");
             //looping through values
 
-            for (var entries : personPhoneNumberHashMap.entrySet()) {
-                System.out.println(entries.getKey());
+        for (Person i : personPhoneNumberHashMap.keySet()) {
+            System.out.println(i);
+        }
+                System.out.println("=====================");
 
                 //looping through keys
 
 
                 personPhoneNumberHashMap.forEach((k, v) -> System.out.println(k + " " + v));
-
+                System.out.println("=====================");
                 //looping through key and values
 
 
                 //Printing methods
                 HashMapMethods.PrintKey(personPhoneNumberHashMap);
+                System.out.println("=====================");
 
                 HashMapMethods.PrintValues(personPhoneNumberHashMap);
+                System.out.println("=====================");
 
                 HashMapMethods.PrintKeysandValues(personPhoneNumberHashMap);
 
 
 
+
+
             }
         }
-    }
-}
-
-
-
 
 
